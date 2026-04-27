6255d118d12cb6b4a43bb0f235570ad4e02ec106
Vladimir Revyagin <skorabeyf@gmail.com>
2026-04-27 22:03:30 +0300

    fix: deploy1

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,15 @@ import type { NextConfig } from 'next'
 import path from 'path'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
-    // prependData: `@use 'variables' as *;`,
   },
 
-  // images: {
-  //   domains: ['xn--e1aedkmddr7af0f.xn--p1ai'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: '83.147.245.180',
-  //       port: '',
-  //       pathname: '**',
-  //     },
-  //   ],
-  // },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 
   async redirects() {
     return [
